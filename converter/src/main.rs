52f130d0f21da2b130c19f0d255e2d762b80b560
fiveseven-lambda <fiveseven.lambda@gmail.com>
2020-12-03 09:28:49 +0900

    converter 書き換え中

--- converter/src/main.rs
+++ converter/src/main.rs
@@ -1,254 +1,230 @@
-use std::collections::HashMap;
-use indexmap::set::IndexSet;
-use std::io::{Write, BufWriter};
+use std::collections::BTreeMap;
+use std::error::Error;
+use std::path::PathBuf;
 
-fn main() -> Result<(), std::io::Error> {
-    let mut args = std::env::args();
-    args.next();
-    if let Some(filename) = args.next() {
-        match std::fs::read_to_string(filename) {
-            Ok(source) => {
-                enum State {
-                    Init,
-                    Tag,
-                    TagContent,
-                    Identity,
-                    ToName,
-                    Name,
-                    Group,
-                    Description,
-                }
-                let mut state = State::Init;
-                let mut line = 1;
-                let mut pos = 1;
-                let mut header = Vec::new();
-                let mut desc = Vec::<(String, String, IndexSet<String>, Vec<String>)>::new();
-                let mut identities = HashMap::<String, String>::new();
-                let mut indices = HashMap::<String, usize>::new();
-                let mut cur : Option<usize> = None;
-                let mut cur_identity = None;
-                let mut cur_name = None;
-                let mut cur_group = None;
-                let mut cur_desc = None;
-                for c in source.chars() {
-                    match state {
-                        State::Init => {
-                            if !c.is_whitespace() {
-                                match c {
-                                    '\\' => {
-                                        state = State::Tag;
-                                        header.push((String::new(), String::new()));
-                                    }
-                                    '+' => {
-                                        match cur {
-                                            Some(cur) => {
-                                                cur_desc = Some(desc[cur].3.len());
-                                                desc[cur].3.push(String::new());
-                                            }
-                                            None => {
-                                                println!("line {}, pos {}: ", line, pos);
-                                            }
-                                        }
-                                        cur_group = Some(String::new());
-                                        state = State::Group;
-                                    }
-                                    _ => {
-                                        cur_identity = Some(c.to_string());
-                                        state = State::Identity;
-                                    }
-                                }
-                            }
+fn main() -> Result<(), Box<dyn Error>> {
+    let files = search_dir("test")?;
+    let source = read(&files)?;
+
+    match Source::from(&source).parse() {
+        Ok(result) => {
+            for expr in &result {
+                match expr {
+                    Expr::Tag(s) => {
+                        print!("Tag: ");
+                        for i in *s {
+                            print!("{}", i.value);
                         }
-                        State::Tag => {
-                            if c == '{' {
-                                state = State::TagContent;
-                            } else {
-                                if let Some(last) = header.last_mut() {
-                                    last.0.push(c);
-                                }
-                            }
-                        }
-                        State::TagContent => {
-                            if c == '}' {
-                                state = State::Init;
-                            } else {
-                                if let Some(last) = header.last_mut(){
-                                    last.1.push(c);
-                                }
-                            }
-                        }
-                        State::Identity => {
-                            if c.is_whitespace() {
-                                if let Some(ref cur_identity) = cur_identity {
-                                    match indices.get(cur_identity) {
-                                        Some(index) => {
-                                            cur = Some(*index);
-                                            state = State::Init;
-                                        }
-                                        None => {
-                                            let len = indices.len();
-                                            indices.insert(cur_identity.to_string(), len);
-                                            cur = Some(len);
-                                            state = State::ToName;
-                                        }
-                                    }
-                                }
-                            } else {
-                                if let Some(cur_identity) = &mut cur_identity {
-                                    cur_identity.push(c);
-                                }
-                            }
-                        }
-                        State::ToName => {
-                            if !c.is_whitespace() {
-                                cur_name = Some(c.to_string());
-                                state = State::Name;
-                            }
-                        }
-                        State::Name => {
-                            if c.is_whitespace() {
-                                if let Some(cur_identity) = &cur_identity {
-                                    if let Some(cur_name) = &cur_name {
-                                        desc.push((cur_identity.to_string(), cur_name.to_string(), IndexSet::new(), Vec::new()));
-                                        identities.insert(cur_name.to_string(), cur_identity.to_string());
-                                    }
-                                }
-                                state = State::Init;
-                            } else {
-                                if let Some(cur_name) = &mut cur_name {
-                                    cur_name.push(c);
-                                }
-                            }
-                        }
-                        State::Group => {
-                            if c == '{' || c.is_whitespace() {
-                                if let Some(cur_group) = &mut cur_group {
-                                    if !cur_group.is_empty(){
-                                        if let Some(cur) = cur {
-                                            desc[cur].2.insert(cur_group.to_string());
-                                        }
-                                    }
-                                    if c != '{' {
-                                        cur_group.clear();
-                                    }
-                                }
-                                if c == '{' {
-                                    state = State::Description;
-                                }
-                            } else {
-                                if let Some(cur_group) = &mut cur_group {
-                                    cur_group.push(c);
-                                }
-                            }
-                        }
-                        State::Description => {
-                            if c == '}' {
-                                state = State::Init;
-                            } else {
-                                if let Some(cur) = cur {
-                                    if let Some(cur_desc) = cur_desc {
-                                        desc[cur].3[cur_desc].push(c);
-                                    }
-                                }
-                            }
-                        }
+                        println!("");
                     }
-                    if c == '\n' {
-                        line += 1;
-                        pos = 1;
-                    } else {
-                        pos += 1;
-                    }
-                }
-                match std::fs::File::create("index.html") {
-                    Ok(out) => {
-                        let mut writer = BufWriter::new(out);
-                        write!(writer, "<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>化合物から見る代謝経路</title><link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"></head><body>")?;
-                        for (tag, content) in header {
-                            write!(writer, "<{}>{}</{}>", tag, content, tag)?;
-                        }
-                        for (identity, name, group, desc) in desc {
-                            write!(writer, "<p class=\"name\" id=\"{}\">{}</p><p class=\"group\">", identity, name)?;
-                            for (i, g) in group.iter().enumerate() {
-                                if !g.is_empty() {
-                                    if i != 0 {
-                                        write!(writer, "・")?;
-                                    }
-                                    write!(writer, "{}", g)?;
-                                }
-                            }
-                            write!(writer, "</p><div class=\"desc\">")?;
-                            for d in desc {
-                                write!(writer, "<p>")?;
-                                enum State {
-                                    Init,
-                                    Escape,
-                                    Link,
-                                    Sub
-                                }
-                                let mut state = State::Init;
-                                let mut link = None;
-                                for c in d.chars() {
-                                    match state {
-                                        State::Init => {
-                                            if c == '$' {
-                                                state = State::Escape;
-                                            } else if !c.is_whitespace() {
-                                                write!(writer, "{}", c)?;
-                                            }
-                                        }
-                                        State::Escape => {
-                                            if c == '[' {
-                                                link = Some(String::new());
-                                                state = State::Link;
-                                            } else if c == '(' {
-                                                state = State::Sub;
-                                                write!(writer, "<sub>(")?;
-                                            }
-                                        }
-                                        State::Link => {
-                                            if c == ']' {
-                                                if let Some(ref link) = link {
-                                                    match identities.get(link) {
-                                                        Some(identity) => {
-                                                            write!(writer, "<a href=\"#{}\">{}</a>", identity, link)?;
-                                                        }
-                                                        None => {
-                                                            write!(writer, "<span class=\"no_link\">{}</span>", link)?;
-                                                            println!("{}?", link);
-                                                        }
-                                                    }
-                                                }
-                                                state = State::Init;
-                                            } else {
-                                                if let Some(link) = &mut link {
-                                                    link.push(c);
-                                                }
-                                            }
-                                        }
-                                        State::Sub => {
-                                            if c == ')' {
-                                                write!(writer, ")</sub>")?;
-                                                state = State::Init;
-                                            } else {
-                                                write!(writer, "{}", c)?;
-                                            }
-                                        }
-                                    }
-                                }
-                                write!(writer, "</p>")?;
-                            }
-                            write!(writer, "</div>")?;
-                        }
-                        write!(writer, "</body>")?;
-                        Ok(())
-                    }
-                    Err(err) => Err(err)
+                    _ => {}
                 }
             }
-            Err(err) => Err(err)
         }
-    } else {
-        Ok(())
+        Err(err) => println!("{}", err),
+    }
+
+    Ok(())
+}
+
+#[derive(thiserror::Error, Debug)]
+enum SearchDirError {
+    #[error("duplicate key (`{0}` and `{1}`)")]
+    DuplicateKey(PathBuf, PathBuf),
+}
+
+fn search_dir(path: &str) -> Result<BTreeMap<usize, PathBuf>, Box<dyn Error>> {
+    let mut ret = BTreeMap::new();
+
+    for entry in std::fs::read_dir(path)? {
+        let path = entry?.path();
+        if path.is_file() {
+            let file_name = path.file_name().ok_or("")?.to_str().ok_or("")?;
+            let num_part = if let Some(index) = file_name.find(|c: char| !c.is_digit(10)) {
+                &file_name[..index]
+            } else {
+                file_name
+            };
+            let key = num_part.parse()?;
+            if let Some(prev) = ret.remove(&key) {
+                return Err(Box::new(SearchDirError::DuplicateKey(prev, path)));
+            } else {
+                ret.insert(key, path);
+            }
+        }
+    }
+
+    Ok(ret)
+}
+
+#[derive(Debug)]
+struct Char {
+    value: char,
+    file: usize,
+    line: usize,
+    pos: usize,
+}
+
+impl Char {
+    fn clone(&self) -> Char {
+        Char {
+            value: self.value,
+            file: self.file,
+            line: self.line,
+            pos: self.pos,
+        }
+    }
+}
+
+fn read(paths: &BTreeMap<usize, PathBuf>) -> Result<Vec<Char>, Box<dyn Error>> {
+    use std::io::prelude::*;
+    let mut ret = Vec::new();
+    for (&i, path) in paths {
+        let file = std::fs::File::open(path)?;
+        for (j, line) in std::io::BufReader::new(file).lines().enumerate() {
+            let mut count = 0usize;
+            for (k, c) in line?.chars().enumerate() {
+                ret.push(Char {
+                    value: c,
+                    file: i,
+                    line: j + 1,
+                    pos: k + 1,
+                });
+                count += 1;
+            }
+            ret.push(Char{ value: '\n', file: i, line: j + 1, pos: count + 1 });
+        }
+    }
+    Ok(ret)
+}
+
+#[derive(Debug)]
+enum Expr<'a> {
+    Tag(&'a [Char]),
+    Name(Text<'a>),
+    Head(&'a [Char], Text<'a>),
+    Desc(&'a [Char], Text<'a>),
+}
+
+#[derive(Debug)]
+enum Text<'a> {
+    Str(&'a [Char]),
+    Block(Vec<Text<'a>>),
+    Link(Vec<Text<'a>>),
+}
+
+struct Source<'a> {
+    source: &'a [Char],
+    iter: std::iter::Enumerate<std::slice::Iter<'a, Char>>,
+}
+
+#[derive(thiserror::Error, Debug)]
+enum ParseError {
+    #[error("at {0:?}, unexpected character")]
+    UnexpectedCharacter(Char),
+    #[error("at {0:?}, brackets do not match")]
+    BracketsDoNotMatch(Char),
+    #[error("unexpected end of file")]
+    UnexpectedEndOfFile,
+}
+
+impl<'a> Source<'a> {
+    fn from(source: &'a [Char]) -> Source<'a> {
+        Source {
+            source: source,
+            iter: source.iter().enumerate(),
+        }
+    }
+
+    fn parse(&mut self) -> Result<Vec<Expr<'a>>, Box<dyn Error>> {
+        let mut ret = Vec::new();
+        enum State<'a> {
+            Space,
+            Tag(usize),
+            Desc(usize),
+            Head(usize),
+            Elem(Expr<'a>),
+        }
+        let mut prev = State::Space;
+        while let Some((i, c)) = self.iter.next() {
+            let next = match prev {
+                State::Desc(index) => match c.value {
+                    '{' => State::Elem(Expr::Desc(
+                        &self.source[index + 1..i],
+                        Text::Block(self.parse_block('}')?),
+                    )),
+                    _ => continue,
+                },
+                State::Head(index) => match c.value {
+                    '{' => State::Elem(Expr::Head(
+                        &self.source[index + 1..i],
+                        Text::Block(self.parse_block('}')?),
+                    )),
+                    _ => continue,
+                },
+                _ => match c.value {
+                    '+' => State::Desc(i),
+                    '\\' => State::Head(i),
+                    '[' => State::Elem(Expr::Name(Text::Link(self.parse_block(']')?))),
+                    c if c.is_whitespace() => State::Space,
+                    c if c.is_alphanumeric() => State::Tag(i),
+                    _ => return Err(Box::new(ParseError::UnexpectedCharacter(c.clone()))),
+                },
+            };
+            match prev {
+                State::Tag(index) => match next {
+                    State::Tag(_) => continue,
+                    _ => ret.push(Expr::Tag(&self.source[index..i])),
+                },
+                State::Elem(elem) => ret.push(elem),
+                _ => {}
+            }
+            prev = next;
+        }
+        match prev {
+            State::Space => {}
+            State::Tag(index) => ret.push(Expr::Tag(&self.source[index..])),
+            State::Desc(_) | State::Head(_) => {
+                return Err(Box::new(ParseError::UnexpectedEndOfFile))
+            }
+            State::Elem(elem) => ret.push(elem),
+        }
+        Ok(ret)
+    }
+
+    fn parse_block(&mut self, delim: char) -> Result<Vec<Text<'a>>, Box<dyn Error>> {
+        let mut ret = Vec::new();
+        enum State<'a> {
+            None,
+            Str(usize),
+            Elem(Text<'a>),
+        }
+        let mut prev = State::None;
+        while let Some((i, c)) = self.iter.next() {
+            let next = match c.value {
+                '{' => State::Elem(Text::Block(self.parse_block('}')?)),
+                '[' => State::Elem(Text::Link(self.parse_block(']')?)),
+                c if c == delim => {
+                    match prev {
+                        State::None => {}
+                        State::Str(index) => ret.push(Text::Str(&self.source[index..i])),
+                        State::Elem(elem) => ret.push(elem),
+                    }
+                    return Ok(ret);
+                }
+                '}' | ']' => return Err(Box::new(ParseError::BracketsDoNotMatch(c.clone()))),
+                _ => State::Str(i),
+            };
+            match prev {
+                State::None => {}
+                State::Str(index) => match next {
+                    State::Str(_) => continue,
+                    _ => ret.push(Text::Str(&self.source[index..i])),
+                },
+                State::Elem(elem) => ret.push(elem),
+            }
+            prev = next;
+        }
+        Err(Box::new(ParseError::UnexpectedEndOfFile))
     }
 }

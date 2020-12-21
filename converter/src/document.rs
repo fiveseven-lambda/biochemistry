badb3326c0f0db2c7c6307dad0d53aa4affffd3a
fiveseven-lambda <fiveseven.lambda@gmail.com>
2020-12-22 07:19:16 +0900

    最終更新日時を書き加えた

--- converter/src/document.rs
+++ converter/src/document.rs
@@ -114,7 +114,7 @@ impl<'a, 'b> Document<'a, 'b> {
     }
 
     pub fn print<Writer: std::io::Write>(&self, mut writer: &mut Writer) -> Result<(), Box<dyn Error>> {
-        write!(writer, "<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>化合物から見る代謝経路</title><link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"></head><body>")?;
+        write!(writer, "<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>化合物から見る代謝経路</title><link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"></head><body><header><h1>化合物から見る代謝経路</h1><p>最終更新日：{}</p></header>", chrono::Local::today().format("%Y/%m/%d"))?;
         for (tag, text) in &self.headers {
             write!(writer, "<{}>", Display::from(tag))?;
             text.print(&mut writer, &self)?;
